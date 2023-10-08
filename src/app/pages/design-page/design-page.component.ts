6d527b59741feb34a5e0e40b79a5ed0e62a33f89
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2023-10-08 20:01:34 -0300

    Actualizacion de Readme

--- src/app/pages/design-page/design-page.component.ts
+++ src/app/pages/design-page/design-page.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-design-page',
   templateUrl: './design-page.component.html',
-  styleUrls: ['./design-page.component.scss']
 })
 export class DesignPageComponent {
 
